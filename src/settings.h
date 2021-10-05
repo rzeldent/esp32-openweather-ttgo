b023a4507023c50f2d7947f27fe2ed83d56c1f70
Rene Zeldenthuis <rene@zeldenthuis.eu>
2021-10-05 22:13:42 +0200

    - Removed NTPClient and Time library. Use stdandard time.h - Added simple message if unable to connect to WiFi - Simplified loop

--- src/settings.h
+++ src/settings.h
@@ -1,17 +1,18 @@
 #pragma once
 
 // The SSID and password of the accesspoint to connect to 
-#define WIFI_SSID       ""
-#define WIFI_PASSWORD   ""
+#define WIFI_SSID       "<fill in your own>"
+#define WIFI_PASSWORD   "<fill in your own>"
 
-// NTP pool to use for the time synchronisation
-#define NTP_POOL        "europe.pool.ntp.org"
-// DST start and end (Central European Standard Time)
+// There must always be 3 NTP servers. Remove or replace by null if not required
+#define NTP_SERVERS     "europe.pool.ntp.org", "0.pool.ntp.org", "1.pool.ntp.org"
+// DST start and end (Central European Standard Time) configure for your timezone
 #define DST_BEGIN       {"CEST", Last, Sun, Mar, 2, 120}
 #define DST_END         {"CET ", Last, Sun, Oct, 3, 60}
-// Synchronize interval in milliseconds (every 30 minutes)
-#define NTP_UPDATE_MILLISECONDS (30 * 60 * 1000)    
 
 // Openweathermap location and subscription id. Create an account at www.openweathermap.org to obtain an id.
 #define OPENWEATHERMAP_LOCATION "Amsterdam"
-#define OPENWEATHERMAP_API_ID   ""
+#define OPENWEATHERMAP_API_ID   "<fill in your own>"
+
+// Every 2 minutes update the display
+#define WEATHER_UPDATE_MILLISECONDS 120000
