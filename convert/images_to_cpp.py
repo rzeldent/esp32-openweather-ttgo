5a530dd7e65c1c6988a7920b347b427313dd37e9
Rene Zeldenthuis <rzeldent@outlook.com>
2021-11-19 20:33:09 +0100

    fixed image conversion script Added pre.py

--- convert/images_to_cpp.py
+++ convert/images_to_cpp.py
@@ -5,11 +5,14 @@ import sys
 from PIL import Image
 
 if (len(sys.argv) < 3):
-    print('Usage: images_to_cpp.py <input_dir> <output_dir>')
+    print('Usage: images_to_cpp.py <input_dir> <output_dir> <header path - optional>')
     sys.exit(1)
 
 input_dir = sys.argv[1]
 output_dir = sys.argv[2]
+header_path = sys.argv[3] if len(sys.argv) > 3 else None
+
+converted = []
 
 file_names = os.listdir(input_dir)
 for file_name in file_names:
@@ -39,12 +42,12 @@ for file_name in file_names:
     output_file.write('\n')
     output_file.write('static const uint16_t ' + image_data_name + '[' + str(height * width) + '] = {\n')
 
-    convert_RGB_to_565 = lambda rgb: (rgb[0] >> 3) << 11 | (rgb[1] >> 2) << 5 | (rgb[2] >> 3)
+    convert_R8G8B8_to_R5G6B5 = lambda rgb: (rgb[0] >> 3) << 11 | (rgb[1] >> 2) << 5 | (rgb[2] >> 3)
 
     for y in range(0, height):
         for x in range(width):
             value = pixels[y * width + x]
-            output_file.write('0x' + hex(convert_RGB_to_565(value))[2:].zfill(4))
+            output_file.write('0x' + hex(convert_R8G8B8_to_R5G6B5(value))[2:].zfill(4))
             if (y*width+x < width*height - 1):
                 output_file.write(',')
         output_file.write('\n')
@@ -56,5 +59,21 @@ for file_name in file_names:
 
     output_file.close()
     image.close()
+
+    converted.append(base_name)
+
+if (header_path):
+    output_file = open(header_path, 'w')
+
+    output_file.write('#pragma once\n')
+    output_file.write('\n')
+    output_file.write('#include <timage.h>\n')
+    output_file.write('\n')
+
+    for base_name in converted:
+        output_file.write('extern const tImage ' + base_name + ';\n')
+
+
+    output_file.close()
 
 print('Done.')
---
+++ include/images.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <timage.h>
+
+extern const tImage image_01d;
+extern const tImage image_01n;
+extern const tImage image_02d;
+extern const tImage image_02n;
+extern const tImage image_03d;
+extern const tImage image_03n;
+extern const tImage image_04;
+extern const tImage image_09;
+extern const tImage image_10d;
+extern const tImage image_10n;
+extern const tImage image_11;
+extern const tImage image_13;
+extern const tImage image_50;
+extern const tImage image_humidity;
+extern const tImage image_no_internet;
+extern const tImage image_openweather;
+extern const tImage image_temperature;
